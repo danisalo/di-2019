7de667c3e8eb396db4a843754c609b7150580478
Daniel Salomon <dsalomo8@pratt.edu>
2019-12-02 13:33:43 -0500

    Update

--- 4.final-project/Crash/sketch.js
+++ 4.final-project/Crash/sketch.js
@@ -1,55 +1,18 @@
-var CrashTable
+// var CrashTable
 
-function preload(){
-  CrashTable = loadTable('data/crashes.csv', 'csv', 'header')
-}
+// function preload(){
+  // CrashTable = loadTable('data/crashesfinal.csv', 'csv', 'header')
+// }
 
 function setup(){
 
 // Setting Canvas
-createCanvas(3000,1600)
+createCanvas(800,800)
 background(250)
 
-var table = CrashTable
-print(table)
+// var table = CrashTable
+// print(table)
 
-////SETTING DATA
-var rowHeight = 25 // Row Letting
-var colWidth = 100 // Column With
+rect(50,50,50,50)
 
-// Typography
-fill(30)
-noStroke()
-textFont("Helvetica Neue")
-textSize(16)
-
-// Categories
-  var x = 350 // Align Left Column
-  var y = 50 // Align Height Column
-  textStyle(BOLD)
-  textAlign(CENTER)
-  for (var c=0; c<table.getColumnCount(); c++){
-    text(table.columns[c], x-colWidth, y)
-    x += colWidth
-
-  }
-
-// let table;
-
-// function preload() {
-//   table = loadTable('data/crashes.csv', 'csv', 'header');
-// }
-
-// function setup() {
-
-//   // Typography
-// fill(30)
-// noStroke()
-// textFont("Helvetica Neue")
-// textSize(16)
-
-//   let tableArray = table.getArray();
-//   for (let i = 0; i < tableArray.length; i++) {
-//     print(tableArray[i]);
-//   }
 }
